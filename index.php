269d2a822cb2966bbf9f38ce021ffff6589e59e3
Jeff Behnke <jeff@valid-webs.com>
2014-05-24 01:35:40 -0500

    New features, common commands, buttons, etc.. see new screenshot

--- index.php
+++ index.php
@@ -102,6 +102,10 @@ $hosts = getHosts( $path );
 		.main .page-header {
 			margin-top: 0;
 		}
+
+		.list-unstyled li {
+			margin: 5px 0 8px 0;
+		}
 	</style>
 	<!--Not really needed-->
 	<!--<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">-->
@@ -146,15 +150,43 @@ $hosts = getHosts( $path );
 			</ul>
 		</div>
 		<div class="col-sm-8 col-sm-offset-4 col-md-9 col-md-offset-3 main">
-			<h1 class="page-header">Current Hosts</h1>
-			<ul class="nav">
-				<?php
-				foreach ( $hosts as $host ) {
-					echo '<li><a href="http://' . $host . '/" target="_blank">' . $host . '</a></li>' . "\n";
-				} // end foreach
-				unset( $host ); ?>
-			</ul>
+			<h1 class="page-header">VVV Dashboard</h1>
 
+			<div class="row">
+				<div class="col-sm-7">
+
+					<p><strong>Current Hosts</strong></p>
+					<small>Note: To profile, <code>xdebug_on</code> must be set.</small>
+					<ul class="list-unstyled">
+						<?php
+						foreach ( $hosts as $host ) {
+							echo '<li class="row">
+							<span class=" col-sm-7">' . $host . '</span>
+
+							<div class=" col-sm-5">
+							<a class="btn btn-primary btn-xs" href="http://' . $host . '/" target="_blank">Load</a>
+							<a class="btn btn-success btn-xs" href="http://' . $host . '/?XDEBUG_PROFILE" target="_blank">Profile</a>
+							</div>
+							</li>' . "\n";
+						} // end foreach
+						unset( $host ); ?>
+					</ul>
+
+				</div>
+				<div class="col-sm-5">
+					<p><strong>Useful Commands</strong></p>
+					<a href="https://github.com/varying-vagrant-vagrants/vvv/#now-what" target="_blank">Commands Link</a>
+					<br />
+					<ul class="list-unstyled">
+					<li><code>vagrant up</code></li>
+						<li><code>vagrant halt</code></li>
+						<li><code>vagrant ssh</code></li>
+						<li><code>vagrant suspend</code></li>
+						<li><code>vagrant resume</code></li>
+						<li><code>xdebug_on</code> <a href="https://github.com/Varying-Vagrant-Vagrants/VVV/wiki/Code-Debugging#turning-on-xdebug" target="_blank">Link</a></li>
+				</ul>
+				</div>
+			</div>
 
 			<h1>If Using <a href="https://github.com/aliso/vvv-site-wizard" target="_blank">VVV Site Wizard</a></h1>
 
@@ -168,68 +200,65 @@ $hosts = getHosts( $path );
 				if a required piece of information is not included in the original command,
 				the wizard will prompt you for it.</p>
 
-			<h3>Options</h3>
-
-			<table class="table table-striped">
-					<thead>
-					<tr>
-						<th>Option</th>
-						<th>Name</th>
-						<th>Description</th>
-					</tr>
-					</thead>
-					<tbody>
-					<tr>
-						<td><code>-a</code></td>
-						<td>action</td>
-						<td>
-							<code>new</code>/<code>create</code>/<code>make</code> runs the site creation wizard,
-							<br /><code>rm</code>/<code>delete</code>/<code>teardown</code> runs the site teardown wizard,
-							<br /><code>list</code> lists all current VVV sites
-						</td>
-					</tr>
-					<tr>
-						<td><code>-d</code></td>
-						<td>domain</td>
-						<td>Desired domain (e.g. mysite.dev)</td>
-					</tr>
-					<tr>
-						<td><code>-f</code></td>
-						<td>files only</td>
-						<td>Do not provision Vagrant, just create the site directory and files</td>
-					</tr>
-					<tr>
-						<td><code>-n</code></td>
-						<td>site name</td>
-						<td>Desired name for the site directory (e.g. mysite)</td>
-					</tr>
-					<tr>
-						<td><code>-p</code></td>
-						<td>path</td>
-						<td>Path to VVV root (e.g. ~/vagrant-local)</td>
-					</tr>
-					<tr>
-						<td><code>-v</code></td>
-						<td>version</td>
-						<td>Version of WordPress to install</td>
-					</tr>
-					<tr>
-						<td><code>-x</code></td>
-						<td>debug</td>
-						<td>Turn on WP_DEBUG and WP_DEBUG_LOG</td>
-					</tr>
-					</tbody>
-				</table>
-
-			<p>Examples:</p>
-
-<pre><code>
-vvv -a list
+<pre><code>vvv -a list
 vvv -a new -n mysite -d mysite.dev -v 3.9.1 -x
 vvv -a delete mysite
 </code></pre>
 
+			<h3>Options</h3>
 
+			<table class="table table-striped">
+				<thead>
+				<tr>
+					<th>Option</th>
+					<th>Name</th>
+					<th>Description</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td><code>-a</code></td>
+					<td>action</td>
+					<td>
+						<code>new</code>/<code>create</code>/<code>make</code> runs the site creation wizard,
+						<br /><code>rm</code>/<code>delete</code>/<code>teardown</code> runs the site teardown wizard,
+						<br /><code>list</code> lists all current VVV sites
+					</td>
+				</tr>
+				<tr>
+					<td><code>-d</code></td>
+					<td>domain</td>
+					<td>Desired domain (e.g. mysite.dev)</td>
+				</tr>
+				<tr>
+					<td><code>-f</code></td>
+					<td>files only</td>
+					<td>Do not provision Vagrant, just create the site directory and files</td>
+				</tr>
+				<tr>
+					<td><code>-n</code></td>
+					<td>site name</td>
+					<td>Desired name for the site directory (e.g. mysite)</td>
+				</tr>
+				<tr>
+					<td><code>-p</code></td>
+					<td>path</td>
+					<td>Path to VVV root (e.g. ~/vagrant-local)</td>
+				</tr>
+				<tr>
+					<td><code>-v</code></td>
+					<td>version</td>
+					<td>Version of WordPress to install</td>
+				</tr>
+				<tr>
+					<td><code>-x</code></td>
+					<td>debug</td>
+					<td>Turn on WP_DEBUG and WP_DEBUG_LOG</td>
+				</tr>
+				</tbody>
+			</table>
+
+			<p>Examples:</p>
 		</div>
 	</div>
 </div>
