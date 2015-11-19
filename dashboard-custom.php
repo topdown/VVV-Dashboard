f932dc490097764b6638224ab2aaf92bc2f4c0d2
Jeff Behnke <jeff@valid-webs.com>
2015-11-19 13:24:19 -0600

    Do a check to see dash base is loaded, if not we redirect.

--- dashboard-custom.php
+++ dashboard-custom.php
@@ -1,9 +1,35 @@
 <?php
 
+/*
+ * -----------------------------------------
+ * SETTINGS
+ * -----------------------------------------
+ */
 
+define( 'VVV_DASH_DEBUG', false );
+
+// Cache settings
+define( 'VVV_DASH_THEMES_TTL', 86400 );
+define( 'VVV_DASH_PLUGINS_TTL', 86400 );
+define( 'VVV_DASH_HOSTS_TTL', 86400 );
+
+
+/**
+ * Redirects to the proper location
+ *
+ * @author         Jeff Behnke <code@validwebs.com>
+ * @copyright  (c) 2009-15 ValidWebs.com
+ *
+ * Created:    11/19/15, 1:12 PM
+ *
+ * @param     $url
+ * @param int $status_code
+ */
 function redirect_to_vvv_dash( $url, $status_code = 301 ) {
 	header( 'Location: ' . $url, true, $status_code );
 	die();
 }
 
-redirect_to_vvv_dash( '/dashboard/index.php', 302 );
+if(! defined('VVV_DASH_BASE')) {
+	redirect_to_vvv_dash( '/dashboard/index.php', 302 );
+}
