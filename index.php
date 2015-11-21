27528512dde7d8e256172364a52951409664ac9b
Jeff Behnke <jeff@valid-webs.com>
2015-11-21 13:24:54 -0600

    HTML fixes

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ define( 'VVV_DASH_VERSION', '0.0.9' );
 
 // Settings
 $path = '../../';
-
 include_once '../dashboard-custom.php';
 include_once 'libs/vvv-dash-cache.php';
 include_once 'libs/functions.php';
@@ -146,113 +145,114 @@ include_once 'views/navbar.php';
 
 	<div class="col-sm-8 col-sm-offset-4 col-md-9 col-md-offset-3 main">
 
-<?php
+		<?php
 
-purge_status( $purge_status );
+		purge_status( $purge_status );
 
-if ( VVV_DASH_VERSION < version_check() ) {
-	?>
-	<div class="alert alert-success alert-dismissible" role="alert">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		<span aria-hidden="true">&times;</span>
-	</button>
-	<p>A new version of <em> VVV Dashboard</em> is available.
-	<br />Your current version: <?php echo VVV_DASH_VERSION ?><br />
-	<strong> New version: <?php echo version_check(); ?></strong></p>
-	</div><?php
-}
-
-
-?>
-
-	<h1 class="page-header">VVV Dashboard</h1>
-
-	<div class="row">
-		<div class="col-sm-12 hosts">
-			<?php
-
-			$close = '<a class="btn btn-primary btn-xs" href="./">Close</a>';
-			if ( ! empty( $plugins ) ) {
-				if ( isset( $_POST['host'] ) ) {
-					?><h4>The plugin list for
-					<span class="red"><?php echo $_POST['host']; ?></span> <?php echo $close; ?></h4><?php
-
-					echo format_table( $plugins, $_POST['host'], 'plugins' );
-				}
-
-			}
-			if ( ! empty( $themes ) ) {
-				if ( isset( $_POST['host'] ) ) {
-					?><h4>The theme list for
-					<span class="red"><?php echo $_POST['host']; ?></span> <?php echo $close; ?></h4><?php
-
-					echo format_table( $themes, $_POST['host'], 'themes' );
-				}
-			}
-
+		if ( VVV_DASH_VERSION < version_check() ) {
 			?>
-			<p>
-				<strong>Current Hosts = <?php echo isset( $hosts['site_count'] ) ? $hosts['site_count'] : ''; ?></strong>
-			</p>
-			<small>Note: To profile, <code>xdebug_on</code> must be set.</small>
+			<div class="alert alert-success alert-dismissible" role="alert">
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+			<p>A new version of <em> VVV Dashboard</em> is available.
+				<br />Your current version: <?php echo VVV_DASH_VERSION ?><br />
+				<strong> New version: <?php echo version_check(); ?></strong></p>
+			</div><?php
+		}
+		?>
 
-			<p class="search-box">Live Search: <input type="text" id="text-search" /></p>
+		<h1 class="page-header">VVV Dashboard</h1>
 
-			<table class="sites table table-responsive table-striped table">
-				<thead>
-				<tr>
-					<th>Debug Mode</th>
-					<th>Sites</th>
-					<th>Actions</th>
-				</tr>
-				</thead>
+		<div class="row">
+			<div class="col-sm-12 hosts">
 				<?php
-				foreach ( $hosts as $key => $array ) {
-					if ( 'site_count' != $key ) { ?>
-						<tr>
-							<?php if ( 'true' == $array['debug'] ) { ?>
-								<td><span class="label label-success">Debug On</span></td>
-							<?php } else { ?>
-								<td><span class="label label-danger">Debug Off</span></td>
-							<?php } ?>
-							<td><?php echo $array['host']; ?></td>
 
-							<td>
-								<a class="btn btn-primary btn-xs" href="http://<?php echo $array['host']; ?>/" target="_blank">Visit Site</a>
+				$close = '<a class="btn btn-primary btn-xs" href="./">Close</a>';
+				if ( ! empty( $plugins ) ) {
+					if ( isset( $_POST['host'] ) ) {
+						?><h4>The plugin list for
+						<span class="red"><?php echo $_POST['host']; ?></span> <?php echo $close; ?></h4><?php
 
-								<?php if ( 'true' == $array['is_wp'] ) { ?>
-									<a class="btn btn-warning btn-xs" href="http://<?php echo $array['host']; ?>/wp-admin" target="_blank">Admin/Login</a>
-								<?php } ?>
-								<a class="btn btn-success btn-xs" href="http://<?php echo $array['host']; ?>/?XDEBUG_PROFILE" target="_blank">Profiler</a>
+						echo format_table( $plugins, $_POST['host'], 'plugins' );
+					}
 
-								<form class="get-themes" action="" method="post">
-									<input type="hidden" name="host" value="<?php echo $array['host']; ?>" />
-									<input type="hidden" name="get_themes" value="true" />
-									<input type="submit" class="btn btn-default btn-xs" name="themes" value="Themes" />
-								</form>
-								<form class="get-plugins" action="" method="post">
-									<input type="hidden" name="host" value="<?php echo $array['host']; ?>" />
-									<input type="hidden" name="get_plugins" value="true" />
-									<input type="submit" class="btn btn-default btn-xs" name="plugins" value="Plugins" />
-								</form>
-							</td>
-						</tr>
-						<?php
+				}
+				if ( ! empty( $themes ) ) {
+					if ( isset( $_POST['host'] ) ) {
+						?><h4>The theme list for
+						<span class="red"><?php echo $_POST['host']; ?></span> <?php echo $close; ?></h4><?php
+
+						echo format_table( $themes, $_POST['host'], 'themes' );
 					}
 				}
-				unset( $array ); ?>
-			</table>
+
+				?>
+				<p>
+					<strong>Current Hosts = <?php echo isset( $hosts['site_count'] ) ? $hosts['site_count'] : ''; ?></strong>
+				</p>
+				<small>Note: To profile, <code>xdebug_on</code> must be set.</small>
+
+				<p class="search-box">Live Search: <input type="text" id="text-search" /></p>
+
+				<table class="sites table table-responsive table-striped table">
+					<thead>
+					<tr>
+						<th>Debug Mode</th>
+						<th>Sites</th>
+						<th>Actions</th>
+					</tr>
+					</thead>
+					<?php
+					foreach ( $hosts as $key => $array ) {
+						if ( 'site_count' != $key ) { ?>
+							<tr>
+								<?php if ( 'true' == $array['debug'] ) { ?>
+									<td><span class="label label-success">Debug On</span></td>
+								<?php } else { ?>
+									<td><span class="label label-danger">Debug Off</span></td>
+								<?php } ?>
+								<td><?php echo $array['host']; ?></td>
+
+								<td>
+									<a class="btn btn-primary btn-xs" href="http://<?php echo $array['host']; ?>/" target="_blank">Visit Site</a>
+
+									<?php if ( 'true' == $array['is_wp'] ) { ?>
+										<a class="btn btn-warning btn-xs" href="http://<?php echo $array['host']; ?>/wp-admin" target="_blank">Admin/Login</a>
+									<?php } ?>
+									<a class="btn btn-success btn-xs" href="http://<?php echo $array['host']; ?>/?XDEBUG_PROFILE" target="_blank">Profiler</a>
+
+									<form class="get-themes" action="" method="post">
+										<input type="hidden" name="host" value="<?php echo $array['host']; ?>" />
+										<input type="hidden" name="get_themes" value="true" />
+										<input type="submit" class="btn btn-default btn-xs" name="themes" value="Themes" />
+									</form>
+									<form class="get-plugins" action="" method="post">
+										<input type="hidden" name="host" value="<?php echo $array['host']; ?>" />
+										<input type="hidden" name="get_plugins" value="true" />
+										<input type="submit" class="btn btn-default btn-xs" name="plugins" value="Plugins" />
+									</form>
+								</td>
+							</tr>
+							<?php
+						}
+					}
+					unset( $array ); ?>
+				</table>
+			</div>
+		</div>
+
+		<div class="error_logs"><h3>Last 10 PHP Errors</h3>
+			<?php
+			$lines = get_php_errors();
+			$lines = format_php_errors( $lines );
+
+			echo $lines;
+
+			include_once 'views/commands-table.php';
+			?>
 		</div>
 	</div>
-
-	<div class="error_logs">        <h3>Last 10 PHP Errors</h3>
 <?php
-$lines = get_php_errors();
-$lines = format_php_errors( $lines );
 
-echo $lines;
-
-?></div><?php
-
-include_once 'views/commands-table.php';
 include_once 'views/footer.php';
--- views/footer.php
+++ views/footer.php
@@ -16,6 +16,9 @@
  * footer.php
  */
 ?>
+<div class="container-fluid">
+
+	<div class="col-sm-8 col-sm-offset-4 col-md-9 col-md-offset-3 main">
 			<p>
 				<strong>NOTE: </strong>This Dashboard project has no affiliation with Varying Vagrant Vagrants or any other components listed here.
 			</p>
@@ -23,8 +26,9 @@
 			<p>
 				<small>VVV Dashboard Version: <?php echo VVV_DASH_VERSION; ?></small>
 			</p>
-		</div>
-	</div>
+
+</div>
+</div>
 </div>
 </body>
 </html>
