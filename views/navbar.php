9d1b1f66c5db4b5139ba258e5f8c57e8ed78b2e7
Jeff Behnke <jeff@valid-webs.com>
2015-11-22 14:50:33 -0600

    License message that should not be there

--- views/navbar.php
+++ views/navbar.php
@@ -6,8 +6,8 @@
  *
  * Created: 11/20/15, 12:26 AM
  *
- * LICENSE: This is PRIVATE source code developed for clients.
- * It is in no way transferable and copy rights belong to Jeff Behnke @ ValidWebs.com
+ * LICENSE:
+ *
  *
  * @author         Jeff Behnke <code@validwebs.com>
  * @copyright  (c) 2015 ValidWebs.com
