121796fcd5e129922851530df1cd1c4e3f4378a1
Jeff Behnke <jeff@validwebs.com>
2015-12-02 10:35:30 -0600

    We need to start refactoring some code

---
+++ libs/vvv-dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ *
+ * PHP version 5
+ *
+ * Created: 12/2/15, 10:33 AM
+ *
+ * LICENSE: This is PRIVATE source code developed for clients.
+ * It is in no way transferable and copy rights belong to Jeff Behnke @ ValidWebs.com
+ *
+ * @author         Jeff Behnke <code@validwebs.com>
+ * @copyright  (c) 2015 ValidWebs.com
+ *
+ * dashboard
+ * vvv-dashboard.php
+ */
+
+class vvv_dashboard {
+
+
+	public function __construct() {
+
+	}
+
+
+	public function __destruct() {
+		// TODO: Implement __destruct() method.
+	}
+}
+// End vvv-dashboard.php
