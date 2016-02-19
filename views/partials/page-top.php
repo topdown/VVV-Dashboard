8f0c9542ca834eb87114de95278bf6fbf3e3d678
Jeff Behnke <jeff@validwebs.com>
2016-02-19 15:44:06 -0600

    Show branch and version info in the head. Useful for dev.

--- views/partials/page-top.php
+++ views/partials/page-top.php
@@ -15,13 +15,16 @@
  * dashboard
  * page-top.php
  */
+$branch = shell_exec( 'cd ' . VVV_WEB_ROOT . '/default/dashboard  && git rev-parse --short HEAD && git rev-parse --abbrev-ref HEAD 2>&1' );
 ?>
 
 	<div class="page-top">
-		<h1 class="page-header"><i class="fa fa-tachometer"></i> VVV Dashboard</h1>
+		<h1 class="page-header"><i class="fa fa-tachometer"></i> VVV Dashboard
+
+		</h1>
 
 		<a class="btn btn-danger btn-sm get-backups" href="?page=backups"> <i class="fa fa-database"></i> Backups</a>
-
+		<p><small>Version: <?php echo VVV_DASH_VERSION . ', Branch: ' . $branch ?></small></p>
 	</div>
 <?php
 // End page-top.php
