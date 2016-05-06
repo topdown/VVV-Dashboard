320bf1446c9cb0f567101e8433a2fec2d87e50c7
Jeff Behnke <jeff@validwebs.com>
2016-05-06 12:19:42 -0500

    Adding some basic info to the about page

--- views/about.php
+++ views/about.php
@@ -16,12 +16,28 @@
  * about.php
  */
 ?>
-<h2>About VVV Dashboard</h2>
+	<h2>About VVV Dashboard</h2>
 
-<a href="https://github.com/topdown/VVV-Dashboard" title="Star the project on Github">
-	<img src="https://img.shields.io/github/stars/topdown/VVV-Dashboard.svg" /></a>
+	<p>
+		<a href="https://github.com/topdown/VVV-Dashboard" title="Star the project on Github"><img src="https://img.shields.io/github/stars/topdown/VVV-Dashboard.svg" /></a>
 
-	<a href="https://github.com/topdown/VVV-Dashboard" title="Fork the project on Github">
-		<img src="https://img.shields.io/github/forks/topdown/VVV-Dashboard.svg" /></a>
+		<a href="https://github.com/topdown/VVV-Dashboard" title="Fork the project on Github"><img src="https://img.shields.io/github/forks/topdown/VVV-Dashboard.svg" /></a>
+
+		<a href="https://gitter.im/topdown/VVV-Dashboard" title="Gitter Chat"><img src="https://badges.gitter.im/Join%20Chat.svg" /></a>
+	</p>
+
+<p>
+	VVV Dashboard goal was to be a simple interface but has now grown into a full development tool for WordPress development.
+</p>
+<p>
+	With a fill list a features to speed up the development process VVV Dashboard has become very powerful and growing with each push/merge.
+</p>
+<p>
+	If there is anything you think should be added or change, create a ticket or a pull request.
+</p>
 <?php
+
+$wpcli_version = shell_exec( 'wp  --info' );
+echo '<p>' . str_replace( "\n", '<br />', $wpcli_version ) . '</p>';
+
 // End about.php
