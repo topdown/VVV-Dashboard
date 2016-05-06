073a9fc916f489b751f5c2b712d636d56d16a291
Jeff Behnke <jeff@validwebs.com>
2016-05-06 10:51:57 -0500

    Missing vvv_dash instance

--- libs/commands/database.php
+++ libs/commands/database.php
@@ -35,7 +35,7 @@ class database {
 
 	/**
 	 *
-	 * @ToDo get rid of $path param
+	 * @ToDo           get rid of $path param
 	 *
 	 * @author         Jeff Behnke <code@validwebs.com>
 	 * @copyright  (c) 2009-15 ValidWebs.com
@@ -50,7 +50,7 @@ class database {
 	public function backup( $path, $file_name ) {
 		$export = shell_exec( 'wp db export --add-drop-table --path=' . $path . ' ' . $file_name );
 
-		if($export) {
+		if ( $export ) {
 			return $export;
 		} else {
 			return false;
@@ -59,7 +59,7 @@ class database {
 
 	/**
 	 *
-	 * @ToDo get rid of $path param
+	 * @ToDo           get rid of $path param
 	 *
 	 * @author         Jeff Behnke <code@validwebs.com>
 	 * @copyright  (c) 2009-15 ValidWebs.com
@@ -74,7 +74,7 @@ class database {
 	public function roll_back( $path, $file ) {
 		$status = shell_exec( 'wp db import --path=' . $path . ' ' . urldecode( $file ) );
 
-		if($status) {
+		if ( $status ) {
 			return $status;
 		} else {
 			return false;
@@ -82,10 +82,11 @@ class database {
 	}
 
 	// @ToDo needs lots of work here, to much happening
-	public function migrate( ) {
+	public function migrate() {
 
 		$host      = $_GET['host'];
 		$domain    = ( isset( $_GET['domain'] ) ) ? $_GET['domain'] : false;
+		$vvv_dash  = new \vvv_dashboard();
 		$host_info = $vvv_dash->set_host_info( $host );
 		$host_path = VVV_WEB_ROOT . '/' . $host_info['host'] . $host_info['path'];
 
@@ -116,6 +117,7 @@ class database {
 
 			return $file . ' was deleted.';
 		}
+
 		return vvv_dash_error( 'ERROR: file does not exist' );
 	}
 
